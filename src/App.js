353549f169dc69caaff5f70d2786b4560b76b6bf
Alex Herlanda <alexisherlanda@gmail.com>
2020-08-14 20:51:31 -0500

    Adds Google ANalytics init on component mount

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Layout } from "antd";
 import MainBar from "./components/molecular/MainBar";
 import EventsFeed from "./pages/EventsFeed";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ReactGA from "react-ga";
 import "./App.less";
 
 function App() {
   const { Header, Content } = Layout;
+
+  useEffect(() => {
+    ReactGA.initialize(process.env.REACT_APP_ANALYTICS_ID);
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
 
   return (
     <Router>

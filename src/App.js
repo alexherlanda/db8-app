b50f03b86ea2347d20d89c360a9d775fc63b5667
alexherlanda <alexisherlanda.mx@gmail.com>
2020-09-19 17:03:32 -0500

    removes block of analytics

--- src/App.js
+++ src/App.js
@@ -8,28 +8,25 @@ import ReactGA from "react-ga";
 import "./App.less";
 import { Element } from "react-scroll";
 
-
 function App() {
   const { Header, Content } = Layout;
 
   useEffect(() => {
-    if (false) {
-      ReactGA.initialize(process.env.REACT_APP_ANALYTICS_ID);
-      ReactGA.pageview(window.location.pathname + window.location.search);
-    }
+    ReactGA.initialize(process.env.REACT_APP_ANALYTICS_ID);
+    ReactGA.pageview(window.location.pathname + window.location.search);
   }, []);
 
   return (
     <Router>
       <Layout>
         <Element name="header">
-        <Header style={{ padding: "0 15px" }}>
-          <MainBar />
-        </Header>
+          <Header style={{ padding: "0 15px" }}>
+            <MainBar />
+          </Header>
         </Element>
-        
+
         <Layout>
-          <Content style={{ margin: 20}}>
+          <Content style={{ margin: 20 }}>
             <Switch>
               <Route path="/events/post">
                 <PostEvent />
